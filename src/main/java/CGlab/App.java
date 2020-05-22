6242a18c1516f8a58cee63b273bf2789f3644e52
karolbury98 <karolbury98@gmail.com>
2020-05-22 18:16:32 +0200

    Lab2_zad4

--- src/main/java/CGlab/App.java
+++ src/main/java/CGlab/App.java
@@ -9,21 +9,29 @@ public class App {
     String version = "0.02";
 
     public static void main(String[] args) {
-        if(args.length<4){
-            System.out.println("Proszę podać argumenty: ścieżkę, szerokość i wysokość oraz wybrany algorytm");
+        if(args.length<3){
+            System.out.println("Proszę podać argumenty: ścieżkę, szerokość i wysokość");
             return;
         }
         String dir = args[0];
         int w = Integer.parseInt(args[1]);
         int h = Integer.parseInt(args[2]);
 
-        Renderer.LineAlgo line=null;
-        if(args[3].equalsIgnoreCase("LINE_NAIVE"))
-            line= Renderer.LineAlgo.NAIVE;
+      //  Renderer.LineAlgo line=null;
+      //  if(args[3].equalsIgnoreCase("LINE_NAIVE"))
+      //      line= Renderer.LineAlgo.NAIVE;
 
         Renderer mainRenderer = new Renderer(dir,w,h);
         mainRenderer.clear();
-        mainRenderer.drawLine(100,100,300,300, line);
+        mainRenderer.drawLine(200,200,400,400, Renderer.LineAlgo.BRESENHAM);
+        mainRenderer.drawLine(200,200,400,200, Renderer.LineAlgo.BRESENHAM);
+        mainRenderer.drawLine(200,200,400,0, Renderer.LineAlgo.BRESENHAM);
+        mainRenderer.drawLine(200,200,200,0, Renderer.LineAlgo.BRESENHAM);
+        mainRenderer.drawLine(200,200,0,0, Renderer.LineAlgo.BRESENHAM);
+        mainRenderer.drawLine(200,200,0,200, Renderer.LineAlgo.BRESENHAM);
+        mainRenderer.drawLine(200,200,0,400, Renderer.LineAlgo.BRESENHAM);
+        mainRenderer.drawLine(200,200,200,0, Renderer.LineAlgo.BRESENHAM);
+
         try {
             mainRenderer.save();
         } catch (IOException ex) {
