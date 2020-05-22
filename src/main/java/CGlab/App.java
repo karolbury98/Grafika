0edf9b0b3fc1febacdb23e8dcad9053530903733
karolbury98 <karolbury98@gmail.com>
2020-05-22 18:01:08 +0200

    Lab2_zad3

--- src/main/java/CGlab/App.java
+++ src/main/java/CGlab/App.java
@@ -9,18 +9,21 @@ public class App {
     String version = "0.02";
 
     public static void main(String[] args) {
-        if(args.length<3){
-            System.out.println("Proszę podać argumenty: ścieżkę, szerokość i wysokość");
+        if(args.length<4){
+            System.out.println("Proszę podać argumenty: ścieżkę, szerokość i wysokość oraz wybrany algorytm");
             return;
         }
         String dir = args[0];
         int w = Integer.parseInt(args[1]);
         int h = Integer.parseInt(args[2]);
 
+        Renderer.LineAlgo line=null;
+        if(args[3].equalsIgnoreCase("LINE_NAIVE"))
+            line= Renderer.LineAlgo.NAIVE;
+
         Renderer mainRenderer = new Renderer(dir,w,h);
         mainRenderer.clear();
-       // mainRenderer.drawPoint(100, 100);
-        mainRenderer.drawLine(0,0,300,300, Renderer.LineAlgo.NAIVE);
+        mainRenderer.drawLine(100,100,300,300, line);
         try {
             mainRenderer.save();
         } catch (IOException ex) {
